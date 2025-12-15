a4a06303c07fb8d97801e2a0ca88b5d34ad4d578
nj <nj@campus.technion.ac.il>
2025-12-15 14:21:56 +0200

    progress

--- eNFA_Regex_equivalence.lean
+++ eNFA_Regex_equivalence.lean
@@ -35,30 +35,68 @@ theorem Regex_to_εNFA (r: RegularExpression alphabet) : ∃ (A: εNFA alphabet 
         rw [@Language.ext_iff]
         intro x
         rw [@mem_singleton_iff]
-
-        have accepts_non_empty : A.accept ∩ (A.eval []) ≠ ∅ := by
-            unfold εNFA.eval εNFA.evalFrom
-            simp
-            rw [@singleton_inter_eq_empty]
-            push_neg
-            constructor
-            exact rfl
+        rw[A.mem_accepts_iff_exists_path]
 
         constructor
         case mp =>
             intro hx
-            subst hx
-            unfold εNFA.accepts
-            simp_all only [εNFA.eval_nil, ne_eq, singleton_inter_eq_empty, not_not, mem_singleton_iff, exists_eq_left, A]
-            exact accepts_non_empty
+            use 0, 0, []
+            exact ⟨ rfl, rfl, id (Eq.symm hx), (εNFA.isPath_nil A).mpr rfl ⟩
+
         case mpr =>
-            intro hx
+            rintro ⟨ q_start, q_accept, x', h_q_start, h_q_accept, h_x', h_ispath ⟩
 
             induction x
-            case nil => tauto
+            case nil => rfl
+            case cons σ tail _ =>
+                absurd h_ispath
+                intro h
+                induction h
+                case nil =>
+                    simp_all only [εNFA.eval_nil, ne_eq, singleton_inter_eq_empty, not_not, mem_singleton_iff,
+                      List.reduceOption_nil, List.nil_eq, reduceCtorEq, A]
+                case cons _ _ _ _ h_step_with_σ _ _ _ =>
+                    exact h_step_with_σ
+
+    case char σ =>
+        let A : εNFA alphabet ℕ := {
+            start  := {0}
+            accept := {1}
+            step   := fun q a => match q, a with
+                | 0, σ => {1}
+                | _, _ => ∅
+        }
+        use A
+
+        simp only [RegularExpression.matches'_char]
+        rw [@Language.ext_iff]
+        intro x
+        rw [@mem_singleton_iff]
+        rw[A.mem_accepts_iff_exists_path]
+
+        constructor
+        case mp =>
+            intro hx
+            use 0, 1, [σ]
+            exact ⟨ rfl, rfl, id (Eq.symm hx), εNFA.IsPath.singleton A rfl ⟩
+
+        case mpr =>
+            -- TODO!!!!!!!!!!!
+            rintro ⟨ q_start, q_accept, x', h_q_start, h_q_accept, h_x', h_ispath ⟩
+
+            induction x
+            case nil =>
+                absurd h_ispath
+                intro h
+                induction h
+                case nil =>
+                    simp_all only [εNFA.eval_nil, ne_eq, singleton_inter_eq_empty, not_not, mem_singleton_iff,
+                      List.reduceOption_nil, List.nil_eq, reduceCtorEq, A]
+                case cons q_start q_step q_accept σ x h₁ h₂ h₃ =>
+
+
             case cons σ tail _ =>
                 absurd hx
-                rw[A.mem_accepts_iff_exists_path]
                 push_neg
                 intro q_start q_accept x' h_q_start h_q_accept h_x'
                 intro h
@@ -68,41 +106,6 @@ theorem Regex_to_εNFA (r: RegularExpression alphabet) : ∃ (A: εNFA alphabet 
                       List.reduceOption_nil, List.nil_eq, reduceCtorEq, A]
                 case cons q_start q_step q_accept σ x h₁ h₂ h₃ =>
                     exact h₁
-
-                -- have h_empty_σ : A.evalFrom A.start [σ] = ∅ := by
-                --     simp_all [A]
-                --     ext x : 1
-                --     simp_all only [εNFA.mem_stepSet_iff, mem_singleton_iff, εNFA.εClosure_empty, mem_empty_iff_false,
-                --       and_false, exists_const, A]
-
-                -- have h_empty_tail : A.evalFrom (A.evalFrom A.start [σ]) tail = ∅ := by
-                --     rw[h_empty_σ]
-                --     exact εNFA.evalFrom_empty A tail
-
-                -- have h_eval : A.eval (σ :: tail) = A.evalFrom (A.evalFrom A.start [σ]) tail := by
-
-                -- have h_empty_x : A.eval (σ :: tail) = ∅ := by
-
-
-                --     sorry
-
-                -- have h_empty_implies_not_accepted: ∀(x : List alphabet), A.eval x = ∅ → x ∉ A.accepts := by
-                --     intro x
-                --     rw[εNFA.accepts, εNFA.eval]
-                --     intro h_x_empty
-                --     have : ∀ S ∈ A.accept, S ∉ A.evalFrom A.start x := by
-                --         exact fun S a => of_eq_false (congrFun h_x_empty S)
-
-                --     have lean_is_stupid: ∀ S ∈ A.accept, S ∉ A.evalFrom A.start x → x ∉ {x | ∃ S ∈ A.accept, S ∈ A.evalFrom A.start x} := by
-                --         simp only [mem_setOf_eq, not_exists, not_and, A]
-
-                --     exact lean_is_stupid 0 rfl (this 0 rfl)
-
-                -- exact h_empty_implies_not_accepted (σ :: tail) h_empty_x
-
-
-    case char =>
-        sorry
     case plus =>
         sorry
     case comp =>
